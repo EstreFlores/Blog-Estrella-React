d47adcabc0f46e53fcf9e8bf79f06b1674636c2c
Estrella Iveth Flores Lucas <estrellaflores550@gmail.com>
2025-08-11 22:06:17 -0600

    corrigiendo para mostrar mi pagina

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://EstreFlores.github.io/Blog-Estrella-React'
+  base:'https://EstreFlores.github.io/Blog-Estrella-React/'
 })
